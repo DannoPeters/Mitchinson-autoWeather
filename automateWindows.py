f0a2c2bc6449fe239143a6fc48f7d99292bdfe74
Danno Peters <Danno306@gmail.com>
2019-10-30 17:00:20 -0600

    max/min instead of open/close added GFA

--- automateWindows.py
+++ automateWindows.py
@@ -10,31 +10,37 @@ except ImportError:
 
 #Change the web adresses below to change where the browser will go to
 liveRVR = "https://atm.navcanada.ca/atm/iwv/CYXE"
-#liveGFA = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/Latest-gfacn32_cldwx_000-e.html?Produit=GFA&Region=32&Langue=anglais&NoSession=NS_Inconnu&Mode=graph"
+liveGFA = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/Latest-gfacn32_cldwx_000-e.html?Produit=GFA&Region=32&Langue=anglais&NoSession=NS_Inconnu&Mode=graph"
 
 #CHnage the times below to chnage the dwell time
-dwellSchedule = 10
-dwellRVR = 10
-dwellGFA = 10
+dwellSchedule = 20
+dwellRVR = 20
+dwellGFA = 20
 
 #choose your favourite web browser
 
-while 1:
-	try:
-		driver = webdriver.firefox.webdriver.WebDriver()
-	except:
-		sys.exit("\tError: Firefox driver is required. Please install 'geckodriver' from https://github.com/mozilla/geckodriver or download pip install webdriverdownloader and run 'webdriverdownloader firefox' and update PATH.")
+try:
+	rvr = webdriver.firefox.webdriver.WebDriver()
+	gfa = webdriver.firefox.webdriver.WebDriver()
+except:
+	sys.exit("\tError: Firefox driver is required. Please install 'geckodriver' from https://github.com/mozilla/geckodriver or download pip install webdriverdownloader and run 'webdriverdownloader firefox' and update PATH.")
 
-	driver.get(liveRVR)
-	driver.maximize_window()
+rvr.get(liveRVR)
+gfa.get(liveGFA)
+
+while 1:
+	#show RVR
+	rvr.maximize_window()
+	gfa.minimize_window()
 	time.sleep(dwellRVR)
 
-	#driver.quit()
-	#driver.get(liveGFA)
-	#driver.maximize_window()
-	#time.sleep(dwellGFA)
-
-	driver.quit()
-	#dwell on the schedule in the background
+	#show GFA
+	rvr.minimize_window()
+	gfa.maximize_window()
+	time.sleep(dwellGFA)
+	
+	#Show Schedule (background)
+	rvr.minimize_window()
+	gfa.minimize_window()
 	time.sleep(dwellSchedule)
 
