78ff6947a5cebd945cf78e929f8e1045e569bc52
Danno Peters <Danno306@gmail.com>
2019-11-01 11:41:51 -0600

    Added local METARs

--- autoWeather.py
+++ autoWeather.py
@@ -10,37 +10,60 @@ except ImportError:
 
 #Change the web adresses below to change where the browser will go to
 liveRVR = "https://atm.navcanada.ca/atm/iwv/CYXE"
-liveGFA = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/Latest-gfacn32_cldwx_000-e.html?Produit=GFA&Region=32&Langue=anglais&NoSession=NS_Inconnu&Mode=graph"
+liveGFA = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/produits/uprair/gfa/gfacn32/Latest-gfacn32_cldwx_000.png"
+southMETAR = "https://flightplanning.navcanada.ca/cgi-bin/route.cgi?Langue=anglais&TypeBrief=R&TypeTriage=R&NoSession=NS_Inconnu&Version=T&Corridor=50&Depart=CYXE&Location=F&EnRoute=CYYN&Location2=C&Destination=CYQR&Location3=P&Alternates=CYKY&Location4=T&SauveReq=actif&cw_metar=raw_metar"
+northMETAR = "https://flightplanning.navcanada.ca/cgi-bin/route.cgi?Langue=anglais&TypeBrief=R&TypeTriage=R&NoSession=NS_Inconnu&Version=T&Corridor=50&Depart=CYXE&Location=F&EnRoute=CYLL&Location2=C&Destination=CYQW&Location3=P&Alternates=CYPA&Location4=T&SauveReq=actif&cw_metar=raw_metar"
 
 #CHnage the times below to chnage the dwell time
 dwellSchedule = 20
 dwellRVR = 20
 dwellGFA = 20
+dwellMETAR = 20
 
 #choose your favourite web browser
 
 try:
 	rvr = webdriver.firefox.webdriver.WebDriver()
 	gfa = webdriver.firefox.webdriver.WebDriver()
+	north = webdriver.firefox.webdriver.WebDriver()
+	south = webdriver.firefox.webdriver.WebDriver()
 except:
 	sys.exit("\tError: Firefox driver is required. Please install 'geckodriver' from https://github.com/mozilla/geckodriver or download pip install webdriverdownloader and run 'webdriverdownloader firefox' and update PATH.")
 
 rvr.get(liveRVR)
 gfa.get(liveGFA)
+north.get(northMETAR)
+south.get(southMETAR)
 
 while 1:
 	#show RVR
 	rvr.maximize_window()
 	gfa.minimize_window()
+	south.minimize_window()
+	north.minimize_window()
 	time.sleep(dwellRVR)
 
 	#show GFA
+	gfa.get(liveGFA) 
 	rvr.minimize_window()
 	gfa.maximize_window()
+	south.minimize_window()
+	north.minimize_window()
 	time.sleep(dwellGFA)
-	
+
 	#Show Schedule (background)
 	rvr.minimize_window()
 	gfa.minimize_window()
+	south.minimize_window()
+	north.minimize_window()
 	time.sleep(dwellSchedule)
+
+	#Show Metars
+	north.get(northMETAR)
+	south.get(southMETAR)
+	south.maximize_window()
+	north.maximize_window()
+	rvr.minimize_window()
+	gfa.minimize_window()
+
 
