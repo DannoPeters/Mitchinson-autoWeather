17be491554374b1f4c40e975a990eb5aa2ff44dc
Danno Peters <Danno@DannoPeters.ca>
2020-02-20 16:10:23 -0600

    Combined GFAs

--- autoWeather.py
+++ autoWeather.py
@@ -3,7 +3,7 @@ import sys
 try:
 	from selenium import webdriver
 except ImportError:
-    sys.exit("\tError: Selenium is required. please install using 'pip install selenium' or download from https://pypi.org/project/selenium/")
+    sys.exit("\tError1: Selenium is required. please install using 'pip install selenium' or download from https://pypi.org/project/selenium/")
 
 
 
@@ -23,6 +23,9 @@ liveTIF[0] = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/produits/up
 liveTIF[1] = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/produits/uprair/gfa/gfacn32/Latest-gfacn32_turbc_006.png"
 liveTIF[2] = "https://flightplanning.navcanada.ca/Latest/gfa/anglais/produits/uprair/gfa/gfacn32/Latest-gfacn32_turbc_012.png"
 
+#Combined GFA TIF
+graphWX = "https://dannopeters.ca/graphical-weather/"
+
 #Depart=CYXE&Location=F&EnRoute=CYYN&Location2=C&Destination=CYQR&Location3=P&Alternates=CYKY&Location4
 #Chnage 4 letter airport acronyms to change dislayed METARs and TAFs 
 southMETAR = "https://flightplanning.navcanada.ca/cgi-bin/route.cgi?Langue=anglais&TypeBrief=R&TypeTriage=R&NoSession=NS_Inconnu&Version=T&Corridor=50&Depart=CYXE&Location=F&EnRoute=CYYN&Location2=C&Destination=CYQR&Location3=P&Alternates=CYKY&Location4=T&SauveReq=actif&cw_metar=raw_metar"
@@ -39,20 +42,20 @@ dwellMETAR = 30
 try:
 	rvr = webdriver.firefox.webdriver.WebDriver()
 
-	gfa = [webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver()]
-
-	tif = [webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver()]
+	#gfa = [webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver()]
+	wx = webdriver.firefox.webdriver.WebDriver()
+	#tif = [webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver(), webdriver.firefox.webdriver.WebDriver()]
 
 	north = webdriver.firefox.webdriver.WebDriver()
 	south = webdriver.firefox.webdriver.WebDriver()
-except:
-	sys.exit("\tError: Firefox driver is required. Please install 'geckodriver' from https://github.com/mozilla/geckodriver or download pip install webdriverdownloader and run 'webdriverdownloader firefox' and update PATH.")
+except Exception as e:
+	sys.exit('\tError: Firefox driver is required. Please install "geckodriver" from https://github.com/mozilla/geckodriver or "pip install webdriverdownloader" and run "webdriverdownloader firefox" and update PATH.\n\n' + str(e))
 
 #open all web addresses
-loop = [0,1,2]
-for i in loop:
-	gfa[i].get(liveGFA[i])
-
+#loop = [0,1,2]
+#for i in loop:
+#	gfa[i].get(liveGFA[i])
+wx.get(graphWX)
 rvr.get(liveRVR)
 north.get(northMETAR)
 south.get(southMETAR)
@@ -62,27 +65,29 @@ while 1:
 	rvr.maximize_window()
 	south.minimize_window()
 	north.minimize_window()
-	for i in loop:
-		gfa[i].minimize_window()
+	wx.minimize_window()
+	#for i in loop:
+	#	gfa[i].minimize_window()
 	time.sleep(dwellRVR)
 
 	#show GFA
 	rvr.minimize_window()
 	south.minimize_window()
 	north.minimize_window()
-	for i in loop:
+	wx.maximize_window()
+	#for i in loop:
 		#Graphic Area Forecast
-		gfa[i].get(liveGFA[i])
-		gfa[i].maximize_window()
-		gfa[i].set_window_size(680,550)
-		gfa[i].set_window_position(0+i*640,0)
+	#	gfa[i].get(liveGFA[i])
+	#	gfa[i].maximize_window()
+	#	gfa[i].set_window_size(680,550)
+	#	gfa[i].set_window_position(0+i*640,0)
 
-	for i in loop:
-		#Turbulance Icing and Freezing
-		tif[i].get(liveTIF[i])
-		tif[i].maximize_window()
-		tif[i].set_window_size(680,550)
-		tif[i].set_window_position(0+i*640,520)
+	#for i in loop:
+	#	#Turbulance Icing and Freezing
+	#	tif[i].get(liveTIF[i])
+	#	tif[i].maximize_window()
+	#	tif[i].set_window_size(680,550)
+	#	tif[i].set_window_position(0+i*640,520)
 		
 	time.sleep(dwellGFA)
 
@@ -90,8 +95,9 @@ while 1:
 	rvr.minimize_window()
 	south.minimize_window()
 	north.minimize_window()
-	for i in loop:
-		gfa[i].minimize_window()
+	wx.minimize_window()
+	#for i in loop:
+	#	gfa[i].minimize_window()
 	time.sleep(dwellSchedule)
 
 	#Show Metars
@@ -106,8 +112,9 @@ while 1:
 	north.set_window_position(960,0)
 	north.execute_script("window.scrollTo(0, 1000)")
 	rvr.minimize_window()
-	for i in loop:
-		gfa[i].minimize_window()
+	wx.minimize_window()
+	#for i in loop:
+	#	gfa[i].minimize_window()
 	time.sleep(dwellMETAR)
 
 
